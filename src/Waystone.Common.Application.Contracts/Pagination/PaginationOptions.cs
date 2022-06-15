8ebb3e926c36b318ad30ce8e3b6605a8ea3396d3
william pei <draekien@users.noreply.github.com>
2022-06-15 11:58:59 +1000

    refactor(pagination): update paginated request to use constant options

---
+++ src/Waystone.Common.Application.Contracts/Pagination/PaginationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Waystone.Common.Application.Contracts.Pagination;
+
+internal static class PaginationOptions
+{
+    public const int MinimumLimit = 1;
+    public const int MaximumLimit = 100;
+    public const int DefaultLimit = 10;
+    public const int MinimumCursor = 0;
+}
