0899066ef2fcbd6acbec131a449c75decd593590
William Pei <draekien@users.noreply.github.com>
2022-06-27 10:48:48 +0000

    Merge pull request #14 from draekien-industries/feature/open-telemetry
    feat(open-telemetry): add open telemetry log event enricher

---
+++ src/Waystone.Common.Api/Logging/OpenTelemetryContextLogEventEnricher.cs
@@ -0,0 +1,21 @@
+﻿namespace Waystone.Common.Api.Logging;
+
+using OpenTelemetry.Trace;
+using Serilog.Core;
+using Serilog.Events;
+
+/// <summary>
+/// Log event enricher for OpenTelemetry.
+/// </summary>
+internal class OpenTelemetryContextLogEventEnricher : ILogEventEnricher
+{
+    /// <inheritdoc />
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        SpanContext context = Tracer.CurrentSpan.Context;
+
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("TraceId", context.TraceId.ToHexString()));
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SpanId", context.SpanId.ToHexString()));
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("TraceFlags", context.TraceFlags.ToString()));
+    }
+}
