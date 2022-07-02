7dbbad0922f565fc7ac731f4d89bda70857aff78
william pei <draekien@users.noreply.github.com>
2022-07-02 11:04:55 +1000

    feat(caching): implement redis caching in infrastructure layer

---
+++ src/Waystone.Common.Infrastructure/Caching/RedisCacheConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Waystone.Common.Infrastructure.Caching;
+
+internal class RedisCacheConfiguration
+{
+    public string[] Endpoints { get; init; } = Array.Empty<string>();
+
+    public bool AllowAdmin { get; init; }
+
+    public string? User { get; init; }
+
+    public string? Password { get; init; }
+
+    public string? ClientName { get; init; }
+
+    public int? DefaultDatabase { get; init; }
+
+    public string? ServiceName { get; init; }
+}
