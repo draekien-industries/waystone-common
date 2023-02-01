c74d41627b88f7ef9cf42c83c51510421dd8fe03
William Pei <draekien@users.noreply.github.com>
2023-02-01 13:29:38 +1100

    test(sample): add more integration tests to product controller

--- test/Waystone.Sample.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
+++ test/Waystone.Sample.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
@@ -6,15 +6,44 @@ using Application.Products;
 using Application.Products.CreateProduct;
 using Common.Application.Contracts.Pagination;
 
-public sealed class ProductsControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+public sealed class ProductsControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>, IAsyncLifetime
 {
+    private readonly List<Guid> _createdProducts;
     private readonly WebApplicationFactory<Program> _factory;
     private readonly IFixture _fixture;
 
     public ProductsControllerTests(CustomWebApplicationFactory<Program> factory)
     {
         _fixture = new Fixture();
+        _createdProducts = new List<Guid>();
         _factory = factory;
+    }
+
+    /// <inheritdoc />
+    public async Task InitializeAsync()
+    {
+        HttpClient client = _factory.CreateClient();
+        IEnumerable<CreateProductCommand>? createProductCommands = _fixture.Build<CreateProductCommand>()
+                                                                           .With(x => x.AmountExcludingTax, 100)
+                                                                           .With(x => x.DiscountPercentage, 0.1m)
+                                                                           .With(x => x.TaxPercentage, 0.1m)
+                                                                           .CreateMany(30);
+
+        Uri createUri = new("Products", UriKind.Relative);
+
+        foreach (CreateProductCommand? command in createProductCommands)
+        {
+            HttpResponseMessage createResponse = await client.PostAsJsonAsync(createUri, command);
+            createResponse.EnsureSuccessStatusCode();
+            var product = await createResponse.Content.ReadFromJsonAsync<ProductDto>();
+            _createdProducts.Add(product!.Id);
+        }
+    }
+
+    /// <inheritdoc />
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
     }
 
     [Fact]
@@ -37,6 +66,10 @@ public sealed class ProductsControllerTests : IClassFixture<CustomWebApplication
         response.EnsureSuccessStatusCode();
         response.StatusCode.Should().Be(HttpStatusCode.Created);
         response.Headers.Location.Should().NotBeNull();
+
+        var product = await response.Content.ReadFromJsonAsync<ProductDto>();
+
+        _createdProducts.Add(product!.Id);
     }
 
     [Fact]
@@ -76,6 +109,8 @@ public sealed class ProductsControllerTests : IClassFixture<CustomWebApplication
 
         var expected = await createResponse.Content.ReadFromJsonAsync<ProductDto>();
 
+        _createdProducts.Add(expected!.Id);
+
         // Act
         var actual = await client.GetFromJsonAsync<ProductDto>(createResponse.Headers.Location);
 
@@ -102,18 +137,10 @@ public sealed class ProductsControllerTests : IClassFixture<CustomWebApplication
     {
         // Arrange
         HttpClient client = _factory.CreateClient();
-        CreateProductCommand? createProductCommand = _fixture.Build<CreateProductCommand>()
-                                                             .With(x => x.AmountExcludingTax, 100)
-                                                             .With(x => x.DiscountPercentage, 0.1m)
-                                                             .With(x => x.TaxPercentage, 0.1m)
-                                                             .Create();
-
-        Uri createUri = new("Products", UriKind.Relative);
-        HttpResponseMessage createResponse = await client.PostAsJsonAsync(createUri, createProductCommand);
-        createResponse.EnsureSuccessStatusCode();
 
         // Act
-        HttpResponseMessage deleteResponse = await client.DeleteAsync(createResponse.Headers.Location);
+        HttpResponseMessage deleteResponse =
+            await client.DeleteAsync(new Uri($"/products/{_createdProducts.Last()}", UriKind.Relative));
 
         // Assert
         deleteResponse.EnsureSuccessStatusCode();
@@ -139,17 +166,6 @@ public sealed class ProductsControllerTests : IClassFixture<CustomWebApplication
     {
         // Arrange
         HttpClient client = _factory.CreateClient();
-        CreateProductCommand? createProductCommand = _fixture.Build<CreateProductCommand>()
-                                                             .With(x => x.AmountExcludingTax, 100)
-                                                             .With(x => x.DiscountPercentage, 0.1m)
-                                                             .With(x => x.TaxPercentage, 0.1m)
-                                                             .Create();
-
-        Uri createUri = new("Products", UriKind.Relative);
-        HttpResponseMessage createResponse = await client.PostAsJsonAsync(createUri, createProductCommand);
-        createResponse.EnsureSuccessStatusCode();
-
-        var expected = await createResponse.Content.ReadFromJsonAsync<ProductDto>();
 
         Uri listUri = new("Products", UriKind.Relative);
 
@@ -160,6 +176,27 @@ public sealed class ProductsControllerTests : IClassFixture<CustomWebApplication
         actual.Should().NotBeNull();
         actual!.Links.Should().NotBeNull();
         actual.Links!.Self.Should().BeEquivalentTo(new Uri("/products?limit=10&cursor=0", UriKind.Relative));
-        actual.Results.Should().ContainEquivalentOf(expected);
+        actual.Links.Next.Should().BeEquivalentTo(new Uri("/products?limit=10&cursor=10", UriKind.Relative));
+        actual.Links.Previous.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task GivenMoreThanOnePageOfProducts_WhenListingProductsOnPage2_ThenReturnCorrectPaginationLinks()
+    {
+        // Arrange
+        HttpClient client = _factory.CreateClient();
+
+        Uri listUri = new("/products?limit=15&cursor=15", UriKind.Relative);
+
+        // Act
+        var actual = await client.GetFromJsonAsync<PaginatedResponse<ProductDto>>(listUri);
+
+        // Assert
+        actual.Should().NotBeNull();
+        actual!.Links.Should().NotBeNull();
+
+        actual.Links!.Self.Should().BeEquivalentTo(new Uri("/products?limit=15&cursor=15", UriKind.Relative));
+        actual.Links.Previous.Should().BeEquivalentTo(new Uri("/products?limit=15&cursor=0", UriKind.Relative));
+        actual.Links.Next.Should().BeEquivalentTo(new Uri("/products?limit=15&cursor=30", UriKind.Relative));
     }
 }
